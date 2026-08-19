ae8f425d9769827676512574c83e98f7a3ae12df
SiyaAgrawal95 <siyaagrawal9506@gmail.com>
2026-08-20 03:25:48 +0530

    1910 Remove All Occurrences of a Substring

---
+++ Strings/RemoveOccurrence.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string removeOccurrences(string s, string part) {
+
+    while (s.length() > 0 && s.find(part) < s.length()) {
+
+        s.erase(s.find(part), part.length());
+
+    }
+
+    return s;
+}
+
+int main() {
+
+    string s, part;
+
+    getline(cin, s);
+    getline(cin, part);
+
+    cout << removeOccurrences(s, part);
+
+    return 0;
+}
